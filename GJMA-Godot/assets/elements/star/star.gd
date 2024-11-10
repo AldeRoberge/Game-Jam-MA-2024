f033f4edb8e9920bcea5692f6556305633c354f0
Alexandre D. Roberge <alderoberge@gmail.com>
2024-11-10 16:23:57 -0500

    ptit commit la dsu

--- GJMA-Godot/assets/elements/star/star.gd
+++ GJMA-Godot/assets/elements/star/star.gd
@@ -7,19 +7,16 @@ extends Area2D
 @export var fade_to_white: FadeToWhite
 
 
-
 func _on_body_entered(body: Node2D) -> void:
 	print("STAR Collided with " + str(body))
 	if body is Player:
-		
-		Global.add_score_to_player(body.device_id)
-		
-		print("2222222222222")
-		#($AnimationPlayer as AnimationPlayer).play("taken")
 		sound.play()
 		music.stop()
 		fade_to_white.fade()
-		
-		# pause game
+
 		get_tree().paused = true
+		await get_tree().create_timer(7.5).timeout
+		get_tree().paused = false
+		
+		Global.add_score_to_player(body.device_id)
 	pass # Replace with function body.
