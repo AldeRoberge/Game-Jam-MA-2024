021fd3efedf737b367e331e425789c3313b19042
Alexandre D. Roberge <alderoberge@gmail.com>
2024-11-10 15:24:50 -0500

    added score to star

--- GJMA-Godot/assets/elements/coin/coin.gd
+++ GJMA-Godot/assets/elements/coin/coin.gd
@@ -6,4 +6,6 @@ var taken = false
 func _on_body_enter(body):
 	print("COIN Collided with " + str(body))
 	if not taken and body is Player:
+		# call the collect coin method on the player
+		body.collect_coin()
 		($AnimationPlayer as AnimationPlayer).play("taken")
