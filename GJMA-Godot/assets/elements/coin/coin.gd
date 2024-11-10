b385805f9d8b9b200df9e08553d5e331970bb8c2
Alexandre D. Roberge <alderoberge@gmail.com>
2024-11-10 15:52:03 -0500

    fixed coin

--- GJMA-Godot/assets/elements/coin/coin.gd
+++ GJMA-Godot/assets/elements/coin/coin.gd
@@ -7,5 +7,6 @@ func _on_body_enter(body):
 	print("COIN Collided with " + str(body))
 	if not taken and body is Player:
 		body = body as Player
+		taken = true
 		($AnimationPlayer as AnimationPlayer).play("taken")
 		Global.add_score_to_player(body.device_id, Global.COIN_SCORE)
